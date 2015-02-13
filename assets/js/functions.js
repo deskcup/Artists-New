739ee2631d024bb41afa0ea520ca80bc8ca093e2
Tamal <tadej.bajda@gmail.com>
2015-02-13 21:30:11 +0100

    Artists-Theme Part 7

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,21 @@
 $( document ).ready(function() {
 
-  //
+  smoothScrool(300)
 
 });
+
+
+// smoothScroll function is applied from the document ready function
+function smoothScrool (duration) {
+	$('a[href^="#"]').on('click', function(event) {
+
+		var target = $( $(this).attr('href') );
+
+		if( target.length ) {
+			event.preventDefault();
+				$('html, body').animate({
+					scrollTop: target.offset().top
+				}, duration);
+		}
+	});
+}
