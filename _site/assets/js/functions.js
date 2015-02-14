f81d918ee76059801d5f95604721399a6a118f2d
Tamal <tadej.bajda@gmail.com>
2015-02-14 23:39:02 +0100

    Artists-Theme Part 11

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -1,12 +1,14 @@
 $( document ).ready(function() {
 
   smoothScrool(300)
+  workBelt();
 
 });
 
 
 // smoothScroll function is applied from the document ready function
 function smoothScrool (duration) {
+	
 	$('a[href^="#"]').on('click', function(event) {
 
 		var target = $( $(this).attr('href') );
@@ -17,5 +19,19 @@ function smoothScrool (duration) {
 					scrollTop: target.offset().top
 				}, duration);
 		}
+	});
+}
+
+
+function workBelt () {
+	
+	$('.thumb-unit').click(function() {
+		$('.work-belt').css('left','-100%');
+		$('.work-container').show();
+	});
+
+	$('.work-return').click(function() {
+		$('.work-belt').css('left','0%');
+		$('.work-container').hide(800);
 	});
 }
