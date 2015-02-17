a64689318876d3e6199f400eab7f37b07abd8bbd
Tamal <tadej.bajda@gmail.com>
2015-02-17 17:55:19 +0100

    Artists-Theme One to Finish

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,12 +1,14 @@
 $( document ).ready(function() {
 
   smoothScrool(300);
-  
   workBelt();
-  
   workLoad();
-
   clientStuff();
+
+  $("header h1").fitText(1, { minFontSize: '48px', maxFontSize: '72px' });
+	$(".biglink").fitText(1.3);
+	
+	$('textarea').autosize();
 
 });
 
@@ -31,12 +33,12 @@ function smoothScrool (duration) {
 function workBelt() {
 	
 	$('.thumb-unit').click(function() {
-		$('.work-belt').css('left','-100%');
+		$('.work-belt').addClass("slided");
 		$('.work-container').show();
 	});
 
 	$('.work-return').click(function() {
-		$('.work-belt').css('left','0%');
+		$('.work-belt').addClass("slided");
 		$('.work-container').hide(800);
 	});
 }
@@ -52,7 +54,7 @@ function workLoad() {
 				newTitle = $this.find('strong').text(),
 				newFolder = $this.data('folder'),
 				spinner = '<div class="loader">Loading...</div>',
-				newHTML = '/work/'+ newFolder +'.html';
+				newHTML = 'work/'+ newFolder +'.html';
 		$('.project-load').html(spinner).load(newHTML);
 		$('.project-title').text(newTitle);
 	});
@@ -63,8 +65,9 @@ function clientStuff() {
 
 	$('.client-unit').first().addClass('active-client');
 	$('.client-logo').first().addClass('active-client');
+	$('.clients-mobile-nav span').first().addClass('active-client');
 
-	$('.client-logo').click(function() {
+	$('.client-logo, .clients-mobile-nav span').click(function() {
 
 		var $this = $(this),
 				$siblings = $this.parent().children(),
@@ -107,3 +110,37 @@ function clientStuff() {
 	});
 
 }
+
+
+(function( $ ){
+
+  $.fn.fitText = function( kompressor, options ) {
+
+    // Setup options
+    var compressor = kompressor || 1,
+        settings = $.extend({
+          'minFontSize' : Number.NEGATIVE_INFINITY,
+          'maxFontSize' : Number.POSITIVE_INFINITY
+        }, options);
+
+    return this.each(function(){
+
+      // Store the object
+      var $this = $(this);
+
+      // Resizer() resizes items based on the object width divided by the compressor * 10
+      var resizer = function () {
+        $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+      };
+
+      // Call once to set.
+      resizer();
+
+      // Call on resize. Opera debounces their resize by default.
+      $(window).on('resize.fittext orientationchange.fittext', resizer);
+
+    });
+
+  };
+
+})( jQuery );
